ec54da2dfe47594cb73f6f4f11538f7b5c0332fc
Rahul Singh <Rahul_ryan@hotmail.com>
2017-02-03 01:35:51 +0530

    Update German CC Analysis.R

--- German CC Analysis.R
+++ German CC Analysis.R
@@ -1,7 +1,7 @@
 ### Objective of analysis.. Minimization of risk and maximization of profit on behalf of the bank.
 
 
-### The German Credit Data contains data on 20 variables and the classification whether an applicant is considered a Good or a Bad credit risk for 1000 loan applicants
+### This Credit Data contains data on 20 variables and the classification whether an applicant is considered a Good or a Bad credit risk for 1000 loan applicants
 
 ## Loading the data
 url="http://freakonometrics.free.fr/german_credit.csv"
